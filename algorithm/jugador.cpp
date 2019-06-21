b3c0cacb7acef7694d37863f2011afe1aaaab561
Federico Agustin Sabatini <sabatinifedericoagustin@gmail.com>
2019-06-21 19:49:06 -0300

    fixed player y ahora juega un toque mejor

--- algorithm/jugador.cpp
+++ algorithm/jugador.cpp
@@ -12,6 +12,7 @@ using namespace std;
 std::random_device rd;
 std::mt19937 generator(rd());
 
+//0 significa no hay ficha, 1 significa la ficha es mia, 2 significa la fichja es del enemigo
 using Tablero = vector<vector<int>>;
 using Fichas = vector<int>;
 
@@ -23,6 +24,14 @@ int SCORE_C2 = 100;
 int SCORE_C1 = 300;
 int SCORE_C = 10000;
 
+int SCORE_BLOQUEOS_C3 = 20;
+int SCORE_BLOQUEOS_C2 = 200;
+int SCORE_BLOQUEOS_C1 = 3000;
+
+
+
+int SCORE_C_BLOCK = SCORE_C / 3;
+
 int posiblesC2;
 int posiblesC1;
 int posiblesC;
@@ -30,6 +39,10 @@ int posiblesC;
 int efectivosC2;
 int efectivosC1;
 int efectivosC;
+
+int bloqueosC3;
+int bloqueosC2;
+int bloqueosC1;
 
 void send(const std::string& msg) {
     std::cout << msg << std::endl;
@@ -64,13 +77,17 @@ int puntuarHorizontal(Tablero tablero, Fichas fichas, int coljugada, int m, int 
 
     int contadorEfectivo = 0;
     int contadorPosibles = 0;
+    int contadorBloqueos = 0;
 
     for(int i= coljugada + 1 - c; i < coljugada + c; i++){
+
         if(i >= 0 && i < m){
+
             if(tablero[fichas[coljugada]][i] != 0) {
                 if(tablero[fichas[coljugada]][i] == player) {
                     contadorEfectivo++;
                     contadorPosibles++;
+                    contadorBloqueos = 0;
 
                     if(contadorEfectivo == c-2)
                         efectivosC2++;
@@ -78,12 +95,22 @@ int puntuarHorizontal(Tablero tablero, Fichas fichas, int coljugada, int m, int 
                         efectivosC1++;
                     if(contadorEfectivo == c)
                         efectivosC++;
+
                 }else {
+                    contadorBloqueos++;
                     contadorEfectivo = 0;
                     contadorPosibles = 0;
+
+                    if(contadorBloqueos == c-3)
+                        bloqueosC3++;
+                    if(contadorBloqueos == c-2)
+                        bloqueosC2++;
+                    if(contadorBloqueos == c-1)
+                        bloqueosC1++;
                 }
-            } else
+            } else {
                 contadorPosibles++;
+            }
 
             if(contadorPosibles >= c-2)
                 posiblesC2++;
@@ -95,10 +122,13 @@ int puntuarHorizontal(Tablero tablero, Fichas fichas, int coljugada, int m, int 
     }
 }
 
+
+
 int puntuarVertical(Tablero tablero, Fichas fichas, int coljugada, int m, int n, int c, int p, int player){
 
     int contadorEfectivo = 0;
     int contadorPosibles = 0;
+    int contadorBloqueos = 0;
 
     for(int i= fichas[coljugada] + 1 - c; i < fichas[coljugada] + c; i++){
         if(i >= 0 && i < n){
@@ -106,6 +136,7 @@ int puntuarVertical(Tablero tablero, Fichas fichas, int coljugada, int m, int n,
                 if(tablero[i][coljugada] == player) {
                     contadorEfectivo++;
                     contadorPosibles++;
+                    contadorBloqueos = 0;
 
                     if(contadorEfectivo == c-2)
                         efectivosC2++;
@@ -114,8 +145,16 @@ int puntuarVertical(Tablero tablero, Fichas fichas, int coljugada, int m, int n,
                     if(contadorEfectivo == c)
                         efectivosC++;
                 }else {
+                    contadorBloqueos++;
                     contadorEfectivo = 0;
                     contadorPosibles = 0;
+
+                    if(contadorBloqueos == c-3)
+                        bloqueosC3++;
+                    if(contadorBloqueos == c-2)
+                        bloqueosC2++;
+                    if(contadorBloqueos == c-1)
+                        bloqueosC1++;
                 }
             } else
                 contadorPosibles++;
@@ -135,11 +174,12 @@ int puntuarDiagSE(Tablero tablero, Fichas fichas, int coljugada, int m, int n, i
 
     int contadorEfectivo = 0;
     int contadorPosibles = 0;
+    int contadorBloqueos = 0;
 
     int rowindex = fichas[coljugada] + 1 - c;
     int colindex = coljugada + 1 - c;
 
-    for(int i = 0; i < c; i++){
+    for(int i = 0; i < c * 2; i++){
 
         if(rowindex >= 0 && colindex >= 0 && rowindex < n && colindex < m){
 
@@ -147,6 +187,7 @@ int puntuarDiagSE(Tablero tablero, Fichas fichas, int coljugada, int m, int n, i
                 if(tablero[rowindex][colindex] == player) {
                     contadorEfectivo++;
                     contadorPosibles++;
+                    contadorBloqueos = 0;
 
                     if(contadorEfectivo == c-2)
                         efectivosC2++;
@@ -155,11 +196,22 @@ int puntuarDiagSE(Tablero tablero, Fichas fichas, int coljugada, int m, int n, i
                     if(contadorEfectivo == c)
                         efectivosC++;
                 }else {
+                    contadorBloqueos++;
                     contadorEfectivo = 0;
                     contadorPosibles = 0;
+
+                    if(contadorBloqueos == c-3)
+                        bloqueosC3++;
+                    if(contadorBloqueos == c-2)
+                        bloqueosC2++;
+                    if(contadorBloqueos == c-1)
+                        bloqueosC1++;
                 }
-            } else
+            } else {
                 contadorPosibles++;
+
+
+            }
 
             if(contadorPosibles >= c-2)
                 posiblesC2++;
@@ -177,11 +229,12 @@ int puntuarDiagNE(Tablero tablero, Fichas fichas, int coljugada, int m, int n, i
 
     int contadorEfectivo = 0;
     int contadorPosibles = 0;
+    int contadorBloqueos = 0;
 
     int rowindex = fichas[coljugada] + 1 - c;
     int colindex = coljugada + c;
 
-    for(int i = 0; i < c; i++){
+    for(int i = 0; i < c * 2; i++){
 
         if(rowindex >= 0 && colindex >= 0 && rowindex < n && colindex < m){
 
@@ -189,6 +242,7 @@ int puntuarDiagNE(Tablero tablero, Fichas fichas, int coljugada, int m, int n, i
                 if(tablero[rowindex][colindex] == player) {
                     contadorEfectivo++;
                     contadorPosibles++;
+                    contadorBloqueos = 0;
 
                     if(contadorEfectivo == c-2)
                         efectivosC2++;
@@ -197,8 +251,16 @@ int puntuarDiagNE(Tablero tablero, Fichas fichas, int coljugada, int m, int n, i
                     if(contadorEfectivo == c)
                         efectivosC++;
                 }else {
+                    contadorBloqueos++;
                     contadorEfectivo = 0;
                     contadorPosibles = 0;
+
+                    if(contadorBloqueos == c-3)
+                        bloqueosC3++;
+                    if(contadorBloqueos == c-2)
+                        bloqueosC2++;
+                    if(contadorBloqueos == c-1)
+                        bloqueosC1++;
                 }
             } else
                 contadorPosibles++;
@@ -214,7 +276,6 @@ int puntuarDiagNE(Tablero tablero, Fichas fichas, int coljugada, int m, int n, i
         colindex--;
     }
 }
-
 
 
 int score(Tablero tablero, Fichas fichas){
@@ -244,13 +305,15 @@ int score(Tablero tablero, Fichas fichas){
         cerr << "------" << endl;
         cerr << "     " << endl;
         */    
-
     score += SCORE_POSC2 * posiblesC2;
     score += SCORE_POSC1 * posiblesC1;
     score += SCORE_POSC * posiblesC;
     score += SCORE_C2 * efectivosC2;
     score += SCORE_C1 * efectivosC1;
     score += SCORE_C * efectivosC;
+    score += SCORE_BLOQUEOS_C3 * bloqueosC3;
+    score += SCORE_BLOQUEOS_C2 * bloqueosC2;
+    score += SCORE_BLOQUEOS_C1 * bloqueosC1;
     return score;
 }
 
@@ -263,11 +326,13 @@ int puntuarJugada(Tablero tablero, Fichas fichas, int coljugada, int m, int n, i
     efectivosC1 = 0;
     efectivosC = 0;
 
+    bloqueosC3 = 0;
+    bloqueosC2 = 0;
+    bloqueosC1 = 0;
     puntuarHorizontal(tablero, fichas, coljugada, m, n, c, p, player);
     puntuarVertical(tablero, fichas, coljugada, m, n, c, p, player);
     puntuarDiagSE(tablero, fichas, coljugada, m, n, c, p, player);
     puntuarDiagNE(tablero, fichas, coljugada, m, n, c, p, player);
-
 
     return score(tablero, fichas);
 
@@ -279,16 +344,18 @@ int evaluarTableros(Tablero tablero, Fichas fichas, int columns, int rows, int c
     int mejorCol;
     
     for(int col = 0; col < columns; col++) {
-        
-        tablero[fichas[col]][col] = 1;
-        int puntaje = puntuarJugada(tablero, fichas, col, columns, rows, c, p, player);
 
-        //cerr << "puntaje: " << puntaje << endl;
-        if(mejorPuntaje < puntaje){
-            mejorPuntaje = puntaje;
-            mejorCol = col;
+        if(fichas[col] < rows){
+            tablero[fichas[col]][col] = 1;
+            int puntaje = puntuarJugada(tablero, fichas, col, columns, rows, c, p, player);
+
+            cerr << "col: " << col << " puntaje: " << puntaje << endl;
+            if(mejorPuntaje < puntaje){
+                mejorPuntaje = puntaje;
+                mejorCol = col;
+            }
+            tablero[fichas[col]][col] = 0;
         }
-        tablero[fichas[col]][col] = 0;
     }
 
     return mejorCol;
